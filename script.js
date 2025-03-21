92663b995b23e620e31aebb45e531d332b7171f5
Sifaul-Islam <sifaul1413@gmail.com>
2025-03-22 03:12:18 +0600

    comleted the project

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ function cart(){
     alert("Added to cart successfully");
 }
 function submit(){
-    var name =
+    var name = document.getElementById("name").value ;
+    var op = (name,"thank you for your feedback");
+    document.getElementById("fb").innerHTML = name;
+    document.getElementById("f2").innerHTML = "Thank you for your feedback";
+
 }
